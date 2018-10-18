ac5e946dbfa88746b1fc14f4fbcafac3173109ab
gohnny <gohnny@gmail.com>
2018-10-18 15:48:59 +0300

    Update connect.php

--- ajax/connect.php
+++ ajax/connect.php
@@ -2,8 +2,8 @@
 try {
     $pdo = new PDO(
         'mysql:host=localhost;dbname=testworktrafgid',
-        'root',
-        '111111',
+        '',
+        '',
         [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
     $pdo->exec("set names utf8");
 } catch (PDOException $e) {
